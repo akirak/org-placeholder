d625301326db762e2832014aa96d54aa7bf295bf
Akira Komamura <akira.komamura@gmail.com>
2024-07-14 18:07:42 +0900

    Add lock directory

---
+++ .rice-lock/default/flake.nix
@@ -0,0 +1,67 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    compat = {
+      flake = false;
+      owner = "emacs-compat";
+      repo = "compat";
+      type = "github";
+    };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
+      type = "github";
+    };
+    ht = {
+      flake = false;
+      owner = "Wilfred";
+      repo = "ht.el";
+      type = "github";
+    };
+    org-ql = {
+      flake = false;
+      owner = "alphapapa";
+      repo = "org-ql";
+      type = "github";
+    };
+    org-super-agenda = {
+      flake = false;
+      owner = "alphapapa";
+      repo = "org-super-agenda";
+      type = "github";
+    };
+    ov = {
+      flake = false;
+      owner = "emacsorphanage";
+      repo = "ov";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+    transient = {
+      flake = false;
+      owner = "magit";
+      repo = "transient";
+      type = "github";
+    };
+    ts = {
+      flake = false;
+      owner = "alphapapa";
+      repo = "ts.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
