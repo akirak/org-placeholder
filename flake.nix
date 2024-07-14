a5ccb3b5118392ca82f66b31c29f8274f07ce055
Akira Komamura <akira.komamura@gmail.com>
2024-07-14 12:12:12 +0900

    dev: Switch to rice-config

--- flake.nix
+++ flake.nix
@@ -1,45 +1,9 @@
 {
-  description = "org-placeholder";
-
-  nixConfig.extra-substituters = "https://emacs-ci.cachix.org";
-  nixConfig.extra-trusted-public-keys = "emacs-ci.cachix.org-1:B5FVOrxhXXrOL0S+tQ7USrhjMT5iOPH+QN9q0NItom4=";
-
-  inputs = {
-    gnu-elpa = {
-      url = "git+https://git.savannah.gnu.org/git/emacs/elpa.git?ref=main";
-      flake = false;
-    };
-    melpa = {
-      url = "github:akirak/melpa/org-placeholder";
-      flake = false;
-    };
-    epkgs = {
-      url = "github:emacsmirror/epkgs";
-      flake = false;
-    };
-    emacs = {
-      url = "github:emacs-mirror/emacs";
-      flake = false;
-    };
-
-    nomake = {
-      url = "github:emacs-twist/nomake";
-      inputs.gnu-elpa.follows = "gnu-elpa";
-      inputs.melpa.follows = "melpa";
-      inputs.epkgs.follows = "epkgs";
-      inputs.emacs.follows = "emacs";
-    };
-  };
-
-  outputs =
-    { self
-    , nomake
-    , ...
-    } @ inputs:
-    nomake.lib.mkFlake {
-      src = ./.;
-      localPackages = [
+  outputs = {...}: {
+    elisp-rice = {
+      packages = [
         "org-placeholder"
       ];
     };
+  };
 }
